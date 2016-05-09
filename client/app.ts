73ae5c065dd5f41ac8a2cc5af799bce80c282eee
David Sorrentino <david@weboxstudio.com>
2016-05-09 13:08:18 +0000

    chore(): update angular2-meteor to 0.5.5

--- client/app.ts
+++ client/app.ts
@@ -1,10 +1,8 @@
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 
-import {LoginButtons} from 'angular2-meteor-accounts-ui/login-buttons';
-
 @Component({
-  directives: [LoginButtons],
+  directives: [],
   selector: 'app',
   templateUrl: 'client/app.html',
 })
